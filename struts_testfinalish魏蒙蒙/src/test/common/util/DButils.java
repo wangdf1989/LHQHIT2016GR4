651ff8cde9846455f36c509ca197dc0796600e8c
lhqhit16grsbbaiyunfei <33413920+lhqhit16grsbbaiyunfei@users.noreply.github.com>
2017-12-04 21:22:40 +0800

    Add files via upload

---
+++ struts_testfinalish魏蒙蒙/src/test/common/util/DButils.java
@@ -0,0 +1,40 @@
+package test.common.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DButils {
+	public static Connection getCon(){
+		String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+		String url = "jdbc:sqlserver://localhost:1433;DatabaseName=student";
+		String username = "sa";
+		String password = "123456";
+		Connection con = null;
+		try {
+			Class.forName(driverClass);
+			con = DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+		
+		
+	}
+
+	public static void closeCon(Connection con){
+		if(con != null){
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
+	}
+}
